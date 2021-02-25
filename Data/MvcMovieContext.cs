b1fb622c5db09a7a315f9f30c270de50c361c3c6
Brenton Wallace <76458335+brentonwallace@users.noreply.github.com>
2021-02-26 07:04:53 +1100

    Created Db context class.

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc_movie.Models;
+
+namespace mvc_movie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+
+        }
+
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
