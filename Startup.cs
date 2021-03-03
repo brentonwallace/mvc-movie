cc5af5c9b6dfc4126523b0b7a6fa381ae79c8263
brentonwallace <ws559666@gmail.com>
2021-03-03 22:19:10 +1100

    Fixed errors.

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace mvc_movie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
             services.AddDbContext<MvcMovieContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("MvcMovieContext")));
         }
